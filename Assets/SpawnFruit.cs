7d6969c5d5727dd3e4eca0f563c8350e8495a18a
Edward Ferdian <tedonk03@gmail.com>
2021-02-12 15:03:02 +1300

    fruit spawner destroy objcs and prefabs update

--- Assets/SpawnFruit.cs
+++ Assets/SpawnFruit.cs
@@ -6,6 +6,7 @@ public class SpawnFruit : MonoBehaviour
 {
     public GameObject fruitPrefab;
     public Transform spawnPos;
+    public IngredientID ingredientType;
 
     private void OnMouseDown()
     {
@@ -14,6 +15,18 @@ public class SpawnFruit : MonoBehaviour
 
     public void Spawn()
     {
-        GameObject.Instantiate(fruitPrefab, spawnPos.position, Quaternion.identity);
+        // destroy all existing one
+
+        var fruits = GameObject.FindGameObjectsWithTag("Ingredient");
+
+        //Debug.Log(fruits.Length + "fruits found");
+        foreach (var fruit in fruits)
+        {
+            if (fruit.GetComponent<Ingredient>().id == ingredientType)
+                Destroy(fruit);
+        }
+
+        var randomRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+        GameObject.Instantiate(fruitPrefab, spawnPos.position, randomRotation);
     }
 }
